ea4fd2e3add923442c5c5dd5ee11d50ecfe20ecd
5ssddev <anassaud5sd@gmail.com>
2019-09-02 19:46:20 +0500

    Game built

--- src/index.js
+++ src/index.js
@@ -2,24 +2,69 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => {alert(`Clicked ${this.props.value}`)}}>
-        { this.props.value }
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
   }
-
+  calcWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let lineValue of lines) {
+      const [a, b, c] = lineValue;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  renderSquare(i) {
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+  handleClick = (i) => {
+    const squares = this.state.squares.slice();
+    if (this.calcWinner(squares) || squares[i]) {
+      return null;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
   render() {
-    const status = 'Next player: X';
-
+    let status;
+    const winner = this.calcWinner(this.state.squares);
+    if (winner) {
+      status = `Winner ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -51,7 +96,7 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div style={{paddingLeft: '30px'}}>{`Status: `/* status */}</div>
+          <div style={{ paddingLeft: '30px' }}>{`Status: `/* status */}</div>
           <ol>{`Todo: `/* TODO */}</ol>
         </div>
       </div>
